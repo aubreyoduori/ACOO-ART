2827f491b6e38e3d6353d1017e330a71209b4b46
aubreyoduori <aubreyoduori@gmail.com>
2026-09-09 11:05:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,62 @@
-function scrollGallery(){
-
-document.getElementById("gallery").scrollIntoView({
-
-behavior:"smooth"
-
-});
-
+function scrollGallery() {
+    document.getElementById("gallery").scrollIntoView({
+        behavior: "smooth"
+    });
 }
+
 
 const images = document.querySelectorAll(".gallery img");
 
 images.forEach(image => {
-
-image.addEventListener("click", () => {
-
-alert("Thanks for viewing my artwork!");
-
+    image.addEventListener("click", () => {
+        alert("Thanks for viewing my artwork!");
+    });
 });
 
-});
+
+function changeColor() {
+    document.getElementById("home").style.color = "red";
+}
+
+
+function makeRed() {
+    document.getElementById("home").style.color = "red";
+}
+
+function makeBlue() {
+    document.getElementById("home").style.color = "blue";
+}
+
+function makeGreen() {
+    document.getElementById("home").style.color = "green";
+}
+
+
+function yellowBackground() {
+    document.body.style.backgroundColor = "yellow";
+}
+
+function blueBackground() {
+    document.body.style.backgroundColor = "lightblue";
+}
+
+function whiteBackground() {
+    document.body.style.backgroundColor = "white";
+}
+
+
+function darkMode() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+}
+
+function lightMode() {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+}
+
+
+function changeMessage() {
+    document.getElementById("message").innerHTML =
+        "Thank you for visiting our website";
+}
